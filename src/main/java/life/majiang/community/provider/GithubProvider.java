43a1a83dfa5c09b8ee82e449ae6faf0a1ced0ed1
MeiLinglLuo <464119137@qq.com>
2020-07-21 17:41:39 +0800

    commit_03

---
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -0,0 +1,34 @@
+package life.majiang.community.provider;
+
+
+import life.majiang.community.dto.AccessTokenDTO;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            return string;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
